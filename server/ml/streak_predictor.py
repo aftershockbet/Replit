9ccce671468a91763c8fb351ba96f77c86054127
aftershockbet <aftershockbet@gmail.com>
2025-11-07 14:46:59 +0000

    fix: Final absolute path for model file in Python script to fix Errno 5

--- server/ml/streak_predictor.py
+++ server/ml/streak_predictor.py
@@ -10,7 +10,7 @@ class StreakPredictor:
     def __init__(self):
         self.model = LogisticRegression(random_state=42, max_iter=1000)
         self.is_trained = False
-        self.model_path = 'server/ml/streak_model.pkl'
+        self.model_path = os.path.join(os.getcwd(), 'server', 'ml', 'streak_model.pkl')
         
         if os.path.exists(self.model_path):
             try:
